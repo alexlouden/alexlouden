d80c9248856b3cf43fe78bb4d14d378cc6969ed9
Alex Louden <alex@louden.com>
2023-05-05 17:55:45 +0800

    markdown tables

--- plugins/markdown.py
+++ plugins/markdown.py
@@ -18,37 +18,35 @@ print 'hello'!
 from markdown import markdown
 from markdown.extensions.fenced_code import FencedCodeExtension, FencedBlockPreprocessor
 from markdown.extensions.toc import TocExtension
+from markdown.extensions.tables import TableExtension
+
 
 from cactus.template_tags import register
 
 
 class PrismCodeExtension(FencedCodeExtension):
-
     def extendMarkdown(self, md, md_globals):
-        """ Add FencedBlockPreprocessor to the Markdown instance. """
+        """Add FencedBlockPreprocessor to the Markdown instance."""
         md.registerExtension(self)
 
-        md.preprocessors.add('fenced_code_block',
-                             LanguageClassPreprocessor(md),
-                             ">normalize_whitespace")
+        md.preprocessors.add(
+            "fenced_code_block", LanguageClassPreprocessor(md), ">normalize_whitespace"
+        )
 
 
 class LanguageClassPreprocessor(FencedBlockPreprocessor):
-    """ Override to add language- to code class, to suit Prism syntax highlighting
+    """Override to add language- to code class, to suit Prism syntax highlighting
     Example output: <code style="language-python>
     """
+
     LANG_TAG = ' class="language-%s"'
 
 
 def mymarkdown(content):
     return markdown(
-        content,
-        extensions=[
-            PrismCodeExtension(),
-            TocExtension()
-        ]
+        content, extensions=[PrismCodeExtension(), TocExtension(), TableExtension()]
     )
 
 
 def preBuild(site):
-    register.filter('markdown', mymarkdown)
+    register.filter("markdown", mymarkdown)
